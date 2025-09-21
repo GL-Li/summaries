f4cb81e1f0bf358fb02d710276f15fb40e0dd282
GL-Li <liguanglai@gmail.com>
2025-09-21 17:48:13 -0400

    create file generate_softlinks.sh

---
+++ soft_linked_summaries/generate_softlinks.sh
@@ -0,0 +1,11 @@
+if [[ ! -f 00_python.md ]]; then
+  ln -s $HOME/projects/program-language-learn/python-learn/00_python.md 00_python.md
+fi
+
+if [[ ! -f 00_docker.md ]]; then
+  ln -s $HOME/projects/docker-learn/00_docker.md 00_docker.md
+fi
+
+if [[ ! -f use_docker.md ]]; then
+  ln -s $HOME/projects/docker-learn/use_docker.md use_docker.md
+fi
